0f3c280e92ec384a3c2ab0fd23d95c49372e2367
mrsikarin <42531318+mrsikarin@users.noreply.github.com>
2022-07-21 10:34:47 +0700

    [DEL]

--- asset/Interface/damage.lua
+++
@@ -1,6 +0,0 @@
-print("startup ui")
-local dmg = self:child("dmg")
-
-function self:setDmg(d)
-  dmg:setText(d)
-end
--- plugin/myplugin/entity/player1/server/server_script.lua
+++
@@ -1,3 +0,0 @@
-PackageHandlers:Receive("GetReward", function(player,packet)
-    player:addItem("myplugin/".."gold-coin",100,nil,"reward")
-  end)
